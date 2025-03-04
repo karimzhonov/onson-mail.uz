35e1e280192b597e3e2c2df1899ae57c4bdd850a
x.karimjonov <khtkarimzhonov@gmail.com>
2025-03-04 17:46:03 +0500

    fixes

--- app/components/calendar/Ramazon.vue
+++ app/components/calendar/Ramazon.vue
@@ -8,14 +8,14 @@
     </div>
     <div class="flex flex-row justify-between w-full">
       <div class="flex justify-center items-center md:flex-row flex-col">
-        <h2 class="text-center md:text-2xl text-lg md:font-bold font-normal md:mr-5 mb-1 text-transparent bg-clip-text bg-gradient-to-br from-primary to-[#8cd66a]">{{$t('Время сухура')}}</h2>
+        <h2 class="text-center md:text-2xl text-lg font-bold md:mr-5 mb-1 text-transparent bg-clip-text bg-gradient-to-br from-primary to-[#8cd66a]">{{$t('Время сухура')}}</h2>
         <span class="text-center flex flex-row items-center md:text-5xl text-2xl dark:text-white" style="font-weight: bolder">
           <img src="/icons/sun-up.webp" alt="" width="45" class="mr-1">
           {{ item.time ?? '-' }}
         </span>
       </div>
       <div class="flex justify-center items-center md:flex-row flex-col">
-        <h2 class="text-center md:text-2xl text-lg md:font-bold font-normal md:mr-5 mb-1 text-transparent bg-clip-text bg-gradient-to-br from-primary to-[#8cd66a]">{{$t('Время ифтара')}}</h2>
+        <h2 class="text-center md:text-2xl text-lg font-bold md:mr-5 mb-1 text-transparent bg-clip-text bg-gradient-to-br from-primary to-[#8cd66a]">{{$t('Время ифтара')}}</h2>
         <span class="text-center flex flex-row items-center md:text-5xl text-2xl dark:text-white" style="font-weight: bolder">
           <img src="/icons/sun-down.webp" alt="" width="45" class="mr-1">
           {{ item.end_time ?? '-'  }}
