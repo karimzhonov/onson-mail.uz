d9a07e1cb12694fd28c14eb6420535830c6249eb
x.karimjonov <khtkarimzhonov@gmail.com>
2025-03-04 17:35:22 +0500

    fixes

--- app/components/BannerCalendar.vue
+++ app/components/BannerCalendar.vue
@@ -1,5 +1,15 @@
 <template>
+  <div class="absolute top-0 inset-x-0 h-64 flex items-start">
+    <div
+        class="h-24 w-2/3 bg-gradient-to-br from-primary opacity-20 blur-2xl dark:from-[#570cac] dark:invisible dark:opacity-40">
+    </div>
+    <div
+        class="h-20 w-3/5 bg-gradient-to-r from-[#8cd66a] opacity-40 blur-2xl dark:from-[#670ccf] dark:opacity-40">
+    </div>
+  </div>
+  <AtomsContainer class-name="relative">
     <component  v-for="day in calendar" :key="day.id" :is="type_to_component[day.type]" :item="day" />
+  </AtomsContainer>
 </template>
 
 <script>
@@ -16,7 +26,7 @@ export default {
   },
   components: { Ramazon },
   async mounted() {
-    const { data } = await axios.get(`http://localhost:7000/api/calendar/`)
+    const { data } = await axios.get(`https://api.onson-mail.uz/api/calendar/`)
     this.calendar = data
   }
 };
--- app/components/calendar/Ramazon.vue
+++ app/components/calendar/Ramazon.vue
@@ -1,12 +1,5 @@
 <template>
-  <div class="absolute top-0 inset-x-0 h-64 flex items-start">
-    <div
-        class="h-24 w-2/3 bg-gradient-to-br from-primary opacity-20 blur-2xl dark:from-[#570cac] dark:invisible dark:opacity-40">
-    </div>
-    <div
-        class="h-20 w-3/5 bg-gradient-to-r from-[#8cd66a] opacity-40 blur-2xl dark:from-[#670ccf] dark:opacity-40">
-    </div>
-  </div>
+
   <div class="relative pt-3 pl-3">
     <div aria-hidden="true" class="absolute inset-y-0 w-44 left-0 hidden dark:flex">
       <div
@@ -14,16 +7,16 @@
       </div>
     </div>
     <div class="flex flex-row justify-between w-full">
-      <div class="flex justify-center flex-col ml-3">
-        <h2 style="font-size: 16px; font-weight: 400" class="text-center mb-1">{{$t('Время сухара')}}</h2>
-        <span class="text-center flex flex-row items-center" style="font-size: 25px; font-weight: bolder">
+      <div class="flex justify-center items-center md:flex-row flex-col">
+        <h2 class="text-center md:text-2xl text-lg md:font-bold font-normal md:mr-5 mb-1">{{$t('Время сухура')}}</h2>
+        <span class="text-center flex flex-row items-center md:text-5xl text-2xl" style="font-weight: bolder">
           <img src="/icons/sun-up.webp" alt="" width="45" class="mr-1">
           {{ item.time ?? '-' }}
         </span>
       </div>
-      <div class="flex justify-center flex-col">
-        <h2 style="font-size: 16px; font-weight: 400" class="text-center mb-1">{{$t('Время ифтара')}}</h2>
-        <span class="text-center flex flex-row items-center" style="font-size: 25px; font-weight: bolder">
+      <div class="flex justify-center items-center md:flex-row flex-col">
+        <h2 class="text-center md:text-2xl text-lg md:font-bold font-normal md:mr-5 mb-1">{{$t('Время ифтара')}}</h2>
+        <span class="text-center flex flex-row items-center md:text-5xl text-2xl" style="font-weight: bolder">
           <img src="/icons/sun-down.webp" alt="" width="45" class="mr-1">
           {{ item.end_time ?? '-'  }}
         </span>
