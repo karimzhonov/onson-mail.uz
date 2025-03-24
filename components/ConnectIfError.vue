819f4c0b78bd87b4f62038b4146e63e697ad2a8f
x.karimjonov <khtkarimzhonov@gmail.com>
2025-03-24 17:30:46 +0500

    add support button

--- components/ConnectIfError.vue
+++ components/ConnectIfError.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="p-2 sticky w-full flex !w-screen justify-center bg-orange-400 text-white items-center">
-        <p class="text-wrap">Заметили неисправность на сайте или есть идея для улучшения, сообщите об этом
-            в <a class="text-blue-400" href="https://t.me/onson_mail_support" target="_blank" rel="noopener">Telegram <SquareArrowOutUpRight class="inline size-4" /></a>
+        <p class="text-wrap">{{$t('Заметили неисправность на сайте или есть идея для улучшения, сообщите об этом')}}.
+            <a class="text-blue-400" href="https://t.me/onson_mail_support" target="_blank" rel="noopener">Telegram <SquareArrowOutUpRight class="inline size-4" /></a>
         </p>
     </div>
 </template>
